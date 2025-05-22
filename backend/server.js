a8c08e7cbc7be74397c3e6b1598b75db1c62e2d1
rafimd422 <rafimd2222@gmail.com>
2025-05-22 10:57:04 +0600

    test: checking the route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello From The backend");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Hello From The Test Route");
+});
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
