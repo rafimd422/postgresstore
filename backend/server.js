7cb62b98f3a690b0a3fbded67acd5ee8d44e21cd
rafimd422 <rafimd2222@gmail.com>
2025-05-22 11:03:32 +0600

    chore: add helmet, morgan, and cors middleware for improved security and logging

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
-
+app.use(express.json());
+app.use(cors());
+app.use(helmet()); // helmet is a security middleware that helps you protect app by setting various HTTP headers
+app.use(morgan("dev"));
 app.get("/", (req, res) => {
+  console.log(res.getHeaders());
   res.send("Hello From The backend");
-});
-
-app.get("/test", (req, res) => {
-  res.send("Hello From The Test Route");
 });
 
 app.listen(3000, () => {
