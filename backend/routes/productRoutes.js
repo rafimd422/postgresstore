26a16785bf462ec4de4cc1a4c326757c0b3741f1
rafimd422 <rafimd2222@gmail.com>
2025-05-24 17:01:38 +0600

    chore:setup database with neon and arcjet

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createProduct, getAllProducts } from "../controller/productController";
+import {
+  createProduct,
+  getAllProducts,
+} from "../controller/productController.js";
 
 const router = express.Router();
 router.get("/", getAllProducts);
 
 router.post("/", createProduct);
+
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import helmet from "helmet";
-import morgan from "morgan";
+import morgan from "morgan"; // Morgan is an HTTP request logger middleware for Node.js
 import cors from "cors";
 import dotenv from "dotenv";
-import productRoutes from "./routes/productRoutes";
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -14,10 +15,32 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(helmet()); // helmet is a security middleware that helps you protect app by setting various HTTP headers
+
+// Morgan logs incoming requests to the console in 'dev' format (method, URL, status, response time)
+// Useful for debugging and monitoring API usage
 app.use(morgan("dev"));
 
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
-  console.log("server is running on port: " + PORT);
+async function initDB() {
+  try {
+    await sql`
+    CREATE TABLE IF NOT EXISTS products(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    `;
+    console.log("Db initialized");
+  } catch (error) {
+    console.log("error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("server is running on port: " + PORT);
+  });
 });
