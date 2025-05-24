114a1e7a46a452f0dc8402a6415bada86dfd0ad1
rafimd422 <rafimd2222@gmail.com>
2025-05-24 20:25:53 +0600

    feat: implement createProduct controller with input validation and DB insert

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -9,11 +9,31 @@ export const getProducts = async (req, res) => {
     console.log("fetched products", products);
     res.status(200).json({ success: true, data: products });
   } catch (error) {
-    console.log(error);
+    console.log("Error in getProducts function", error);
+    res.status(500).json({ success: false, message: "Internal server Error" });
   }
 };
 
-export const createProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
+  const { name, price, image } = req.body;
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+  try {
+    const newProduct = await sql`
+    INSERT INTO products(name, price, image)
+    VALUES (${name},${price},${image})  
+    RETURNING *  
+    `;
+    console.log("new product added", newProduct[0]);
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log("Error in createProduct function", error);
+    res.status(500).json({ success: false, message: "Internal server Error" });
+  }
+};
 
 export const getProduct = async (req, res) => {};
 export const updateProduct = async (req, res) => {};
