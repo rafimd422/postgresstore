04a5479b02dbac5a9ac89da531a9f3b521effbc1
rafimd422 <rafimd2222@gmail.com>
2025-05-25 00:09:13 +0600

    feat: implemented functionlaity to get single product by ID

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -35,6 +35,19 @@ export const createProduct = async (req, res) => {
   }
 };
 
-export const getProduct = async (req, res) => {};
+export const getProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await sql`
+    SELECT * FROM products WHERE id=${id}
+    `;
+
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (error) {
+    console.log("Error in getProduct function", error);
+    res.status(500).json({ success: false, message: "Internal server Error" });
+  }
+};
 export const updateProduct = async (req, res) => {};
 export const deleteProduct = async (req, res) => {};
