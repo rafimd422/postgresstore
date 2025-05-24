86afedc90401a67482e9672a6d10511346f2113f
rafimd422 <rafimd2222@gmail.com>
2025-05-24 20:18:02 +0600

    feat: implement getProducts controller with DB integration

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,4 +1,18 @@
-export const getProducts = async (req, res) => {};
+import { sql } from "../config/db.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await sql`
+        SELECT * FROM Products
+        ORDER BY created_at DESC
+        `;
+    console.log("fetched products", products);
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const createProduct = async (req, res) => {};
 
 export const getProduct = async (req, res) => {};
